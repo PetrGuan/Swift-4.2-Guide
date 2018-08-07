4588996d15657d3bf00c622c6938d0948e806110
Peter Guan <gkratos92@gmail.com>
2018-08-07 08:32:43 +0800

    begin journey of CheckList app

---
+++ CheckLists/CheckLists/CheckListViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  CheckLists
+//
+//  Created by 管君 on 8/6/18.
+//  Copyright © 2018 管君. All rights reserved.
+//
+
+import UIKit
+
+class CheckListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CheckListItem", for: indexPath)
+        
+        let label = cell.viewWithTag(1000) as! UILabel
+        if indexPath.row % 5 == 0 {
+            label.text = "Walk the dog"
+        } else if indexPath.row % 5 == 1 {
+            label.text = "Brush my teeth"
+        } else if indexPath.row % 5 == 2 {
+            label.text = "Learn IOS development"
+        } else if indexPath.row % 5 == 3 {
+            label.text = "Soccer practice"
+        } else if indexPath.row % 5 == 4 {
+            label.text = "Eat ice cream"
+        }
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
+}
+
