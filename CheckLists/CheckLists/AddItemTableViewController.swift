0cc6668cf3fc09bb318f78753c65bdcbf7a42b94
Peter Guan <gkratos92@gmail.com>
2018-08-10 20:51:17 +0800

    complete chapter-13

--- CheckLists/CheckLists/AddItemTableViewController.swift
+++ CheckLists/CheckLists/AddItemTableViewController.swift
@@ -8,9 +8,9 @@
 
 import UIKit
 
-class AddItemTableViewController: UITableViewController {
-
+class AddItemTableViewController: UITableViewController, UITextFieldDelegate {
     
+    @IBOutlet weak var doneBarButton: UIBarButtonItem!
     @IBOutlet weak var textField: UITextField!
     
     @IBAction func cancel() {
@@ -36,5 +36,19 @@ class AddItemTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
         return nil
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in:oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        
+        if newText.isEmpty {
+            doneBarButton.isEnabled = false
+        }
+        else {
+            doneBarButton.isEnabled = true
+        }
+        return true
     }
 }
