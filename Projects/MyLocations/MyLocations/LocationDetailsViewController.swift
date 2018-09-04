d20d92f8f6aba57c3745cbff7167541d9556f4bb
Peter Guan <gkratos92@gmail.com>
2018-09-04 22:59:12 +0800

    chapter-25 on my way

--- Projects/MyLocations/MyLocations/LocationDetailsViewController.swift
+++ Projects/MyLocations/MyLocations/LocationDetailsViewController.swift
@@ -7,14 +7,26 @@
 //
 
 import UIKit
+import CoreLocation
+
+private let dateFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .medium
+    formatter.timeStyle = .short
+    return formatter
+}()
 
 class LocationDetailsViewController: UITableViewController {
+    
     @IBOutlet weak var descriptionTextView: UITextView!
     @IBOutlet weak var categoryLabel: UILabel!
     @IBOutlet weak var latitudeLabel: UILabel!
     @IBOutlet weak var longitudeLabel: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
+    
+    var coordinate = CLLocationCoordinate2D(latitude: 0, longitude: 0)
+    var placemark: CLPlacemark?
     
     // MARK:- Actions
     @IBAction func done() {
@@ -23,5 +35,53 @@ class LocationDetailsViewController: UITableViewController {
     
     @IBAction func cancel() {
         navigationController?.popViewController(animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        descriptionTextView.text = ""
+        categoryLabel.text = ""
+        
+        latitudeLabel.text = String(format: "%.8f", coordinate.latitude)
+        longitudeLabel.text = String(format: "%.8f", coordinate.longitude)
+        
+        if let placemark = placemark {
+            addressLabel.text = string(from: placemark)
+        }
+        else {
+            addressLabel.text = "No Address Found"
+        }
+        
+        dateLabel.text = format(date: Date())
+    }
+    
+    // MARK:- Private Methods
+    func string(from placemark: CLPlacemark) -> String {
+        var text = ""
+        
+        if let s = placemark.subThoroughfare {
+            text += s + " "
+        }
+        if let s = placemark.thoroughfare {
+            text += s + " "
+        }
+        if let s = placemark.locality {
+            text += s + " "
+        }
+        if let s = placemark.administrativeArea {
+            text += s + " "
+        }
+        if let s = placemark.postalCode {
+            text += s + " "
+        }
+        if let s = placemark.country {
+            text += s + " "
+        }
+        return text
+    }
+    
+    func format(date: Date) -> String {
+        return dateFormatter.string(from: date)
     }
 }
