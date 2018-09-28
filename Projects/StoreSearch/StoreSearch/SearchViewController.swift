f6cab83c9c94e312a14b1deb0526c7eeba7a177a
Peter Guan <gkratos92@gmail.com>
2018-09-28 08:35:32 +0800

    begin StoreSearch

---
+++ Projects/StoreSearch/StoreSearch/SearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SearchViewController.swift
+//  StoreSearch
+//
+//  Created by 管君 on 9/28/18.
+//  Copyright © 2018 管君. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
