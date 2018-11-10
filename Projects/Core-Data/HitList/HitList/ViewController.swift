2c99b95c77c63f5264e9bc72ec71dba997536f99
Peter Guan <gkratos92@gmail.com>
2018-11-10 09:11:24 +0800

    Coredata Chap-1 complete

--- Projects/Core-Data/HitList/HitList/ViewController.swift
+++ Projects/Core-Data/HitList/HitList/ViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2018 管君. All rights reserved.
 //
 
+import CoreData
 import UIKit
 
 class ViewController: UIViewController {
@@ -22,7 +23,7 @@ class ViewController: UIViewController {
                 return
             }
             
-            self.names.append(nameToSave)
+            self.save(name: nameToSave)
             self.tableView.reloadData()
         }
         
@@ -34,7 +35,7 @@ class ViewController: UIViewController {
         present(alert, animated: true)
     }
     
-    var names: [String] = []
+    var people: [NSManagedObject] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,19 +44,65 @@ class ViewController: UIViewController {
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // 1
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        // 2
+        let fetchResult = NSFetchRequest<NSManagedObject>(entityName: "Person")
+        
+        // 3
+        do {
+            people = try managedContext.fetch(fetchResult)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+    
+    func save(name: String) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        // 1
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        // 2
+        let entity = NSEntityDescription.entity(forEntityName: "Person", in: managedContext)!
+        
+        let person = NSManagedObject(entity: entity, insertInto: managedContext)
+        
+        // 3
+        person.setValue(name, forKey: "name")
+        
+        // 4
+        do {
+            try managedContext.save()
+            people.append(person)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
 }
 
 // MARK:- UITableViewDataSource
 extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return names.count
+        return people.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let person = people[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
-        cell.textLabel?.text = names[indexPath.row]
+        cell.textLabel?.text = person.value(forKey: "name") as? String
         return cell
     }
     
