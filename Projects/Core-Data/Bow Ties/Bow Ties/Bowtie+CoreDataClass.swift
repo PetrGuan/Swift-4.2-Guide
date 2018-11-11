37afe62b8074f3cd8bd52f36a9cab8d8d7aca571
Peter Guan <gkratos92@gmail.com>
2018-11-11 10:43:49 +0800

    Complete Coredata Chap-2

---
+++ Projects/Core-Data/Bow Ties/Bow Ties/Bowtie+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Bowtie+CoreDataClass.swift
+//  Bow Ties
+//
+//  Created by 管君 on 11/10/18.
+//  Copyright © 2018 Razeware. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Bowtie)
+public class Bowtie: NSManagedObject {
+
+}
---
+++ Projects/Core-Data/Bow Ties/Bow Ties/Bowtie+CoreDataProperties.swift
@@ -0,0 +1,29 @@
+//
+//  Bowtie+CoreDataProperties.swift
+//  Bow Ties
+//
+//  Created by 管君 on 11/10/18.
+//  Copyright © 2018 Razeware. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Bowtie {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Bowtie> {
+        return NSFetchRequest<Bowtie>(entityName: "Bowtie")
+    }
+
+    @NSManaged public var isFavorite: Bool
+    @NSManaged public var lastWorn: NSDate?
+    @NSManaged public var name: String?
+    @NSManaged public var photoData: NSData?
+    @NSManaged public var rating: Double
+    @NSManaged public var searchKey: String?
+    @NSManaged public var timesWorn: Int32
+    @NSManaged public var tintColor: NSObject?
+
+}
