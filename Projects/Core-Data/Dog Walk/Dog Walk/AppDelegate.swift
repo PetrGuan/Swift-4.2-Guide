925b2401c7039f66bac17ef056593bb33e9d901d
Peter Guan <gkratos92@gmail.com>
2018-11-12 23:44:42 +0800

    Coredata chap-3 half-way

--- Projects/Core-Data/Dog Walk/Dog Walk/AppDelegate.swift
+++ Projects/Core-Data/Dog Walk/Dog Walk/AppDelegate.swift
@@ -21,13 +21,24 @@
  */
 
 import UIKit
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
+  lazy var coreDataStack = CoreDataStack(modelName: "Dog Walk")
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    guard let navController = window?.rootViewController as? UINavigationController, let viewController = navController.topViewController as? ViewController else {
+      return true
+    }
+    
+    viewController.managedContext = coreDataStack.managedContext
     return true
+  }
+  
+  func applicationWillTerminate(_ application: UIApplication) {
+    coreDataStack.saveContext()
   }
 }
